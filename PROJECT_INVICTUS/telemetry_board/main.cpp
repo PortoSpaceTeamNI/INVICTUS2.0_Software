da5f49f51f94f2398ff7a7c738b798788b04e4ae
AlexSMFerreira <alexandre.smferreira@gmail.com>
2025-02-13 00:11:24 +0000

    testing project structure

---
+++ PROJECT_INVICTUS/telemetry_board/main.cpp
@@ -0,0 +1,14 @@
+#include <Arduino.h>
+
+void setup()
+{
+  // put your setup code here, to run once:
+  Serial.begin(115200); 
+  Serial.print("Hello from the telemetry board!\n");
+}
+
+void loop()
+{
+    // do nothing
+}
+
