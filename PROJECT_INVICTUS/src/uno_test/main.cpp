bc9fde7d88bfa7fd7e2b8b8a40a851c66d09e2c7
Alexandre Ferreira <alexandre.smferreira@gmail.com>
2025-02-13 00:35:29 +0000

    multiple src directories working

---
+++ PROJECT_INVICTUS/src/uno_test/main.cpp
@@ -0,0 +1,14 @@
+#include <Arduino.h>
+
+String line = "";
+
+void setup()
+{
+  // put your setup code here, to run once:
+  Serial.begin(115200);
+  Serial.print("Hello from Arduino Uno!\n");
+}
+
+void loop()
+{
+}
